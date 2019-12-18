96f7879ef5b4584aba282cc948f011d7362e2de6
Slava Semeniuk <svyatoslav.semenyuk@gmail.com>
2019-12-18 12:28:07 +0200

    add age field to item validator

--- validators/itemsValidator.js
+++ validators/itemsValidator.js
@@ -2,6 +2,17 @@ const ajv = require('ajv')()
 
 const itemSchema = {
   type: 'object',
+  properties: {
+    name: {
+      type: 'string',
+      minLength: 2,
+      maxLength: 64
+    },
+    age: {
+      type: 'number',
+      exclusiveMinimum: 0
+    }
+  },
   additionalProperties: false,
   required: ['name'],
 }
